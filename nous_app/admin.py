977ab9050a38b8c38f0ba9802f04c81ad480678d
Uzochukwu Onuegbu <uonuegbu@groove.ng>
2020-04-28 21:08:02 +0100

    started adding styling

--- nous_app/admin.py
+++ nous_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Item
+
+admin.site.site_header = 'NOUS Digital Admin Dashboard'
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ('title', 'image',)
+    save_on_top = True
+
+
+admin.site.register(Item, ItemAdmin)
 
 # Register your models here.
--- nous_app/models.py
+++ nous_app/models.py
@@ -7,3 +7,6 @@ class Item(models.Model):
     title = models.CharField(max_length=400)
     description = models.TextField(max_length=1000)
     image = models.URLField(max_length=300)
+
+    def __str__(self):
+        return self.title
