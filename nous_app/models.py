2424817cbef79fc2dc8938bdb63824e0467bb5b6
Uzochukwu Onuegbu <uonuegbu@groove.ng>
2020-04-28 15:27:09 +0100

    added Item model

--- nous_app/models.py
+++ nous_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    image = models.URLField(max_length=200)
